c0a90197da5340da504686dd9ec9287d9a110da8
Hattyot <hattyot@gmail.com>
2018-08-28 17:20:57 +0300

    temp fix v3

--- events/main/ready.js
+++ events/main/ready.js
@@ -1,13 +1,6 @@
 const query = require("../../modules/query.js")
 module.exports = async (bot) => {
     console.log("Bot ready")
-    function crt() {
-        let sql1 = "CREATE TABLE guilds (guildID VARCHAR(255), AChannel VARCHAR(255))"
-        bot.con.query(sql1, function (err, result) {
-          if (err) throw err;
-          console.log("done")
-        })
-    }
     function crt2() {
         let sql2 = "CREATE TABLE streamers (guildID VARCHAR(255), streamerName VARCHAR(255), streamerID VARCHAR(255))"
         bot.con.query(sql2, function (err, result) {
