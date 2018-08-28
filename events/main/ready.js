c50f7b200ed2eebab5d0d1c7ceb8b2e129826cb2
Hattyot <hattyot@gmail.com>
2018-08-28 17:19:44 +0300

    temp fix v2

--- events/main/ready.js
+++ events/main/ready.js
@@ -5,24 +5,14 @@ module.exports = async (bot) => {
         let sql1 = "CREATE TABLE guilds (guildID VARCHAR(255), AChannel VARCHAR(255))"
         bot.con.query(sql1, function (err, result) {
           if (err) throw err;
-          let embed = new Discord.RichEmbed()
-            .addField("** **",`***:white_check_mark: Table 1 created*** ** **`)
-            .setColor(bot.config.embed.embedColor)
-            .setTimestamp()
-            .setFooter(`${message.author.tag}`, message.author.displayAvatarURL)
-          return message.channel.send(embed)
-        });
+          console.log("done")
+        })
     }
     function crt2() {
         let sql2 = "CREATE TABLE streamers (guildID VARCHAR(255), streamerName VARCHAR(255), streamerID VARCHAR(255))"
         bot.con.query(sql2, function (err, result) {
           if (err) throw err;
-          let embed = new Discord.RichEmbed()
-            .addField("** **",`***:white_check_mark: Table 2 created*** ** **`)
-            .setColor(bot.config.embed.embedColor)
-            .setTimestamp()
-            .setFooter(`${message.author.tag}`, message.author.displayAvatarURL)
-          return message.channel.send(embed)
+          console.log("done")
         });
     }
     crt()
