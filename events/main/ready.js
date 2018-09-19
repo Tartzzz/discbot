a49cbe63c04c8afd619a9a412d4478741c57c06d
Hattyot <39061940+Hattyot@users.noreply.github.com>
2018-09-19 20:58:48 +0300

    Update ready.js

--- events/main/ready.js
+++ events/main/ready.js
@@ -6,33 +6,39 @@ module.exports = async (bot) => {
 
     let SchannelIDS = {}
     let DchannelIDS = {}
-    query.select("guilds", {guildID: guild.id}, result => {
-        query.select("streamers", {all: true, guildID: guild.id}, result => {
-            if(!result) return
-            for(i = 0; i < result.length; i++) {
-                SchannelIDS[result[0].guildID].push(Math.floor(result[i].streamerID))
-            }
-        })
-        SchannelIDS[guild.id] = []
-        DchannelIDS[guild.id] = {}
-        if(!result) return
-        DchannelIDS[result.guildID].aChannel = result.AChannel
-        let guildIDS = Object.keys(SchannelIDS)
-        for(i = 0; i < guildIDS.length; i++) {
-            var channelID = SchannelIDS[guildIDS[i]]
-            var discordChannelID = `${DchannelIDS[guildIDS[i]].aChannel}`
-            var guildID = `${guildIDS[i]}`
+    query.select("guilds", {all: true}, resultG => {
+        if(!resultG) return
+        
+        for(i = 0; i < resultG.length; i++) {
+            guildID = resultG[i].guildID
+            AChannel = resultG[i].AChannel
+            SchannelIDS[guildID] = []
+            DchannelIDS[guildID] = {}
+            DchannelIDS[guildID].aChannel = AChannel
+            query.select("streamers", {all: true, guildID}, result => {
+                if(!result) return
 
-            for(i = 0; i < channelID.length; i++) {
-                let config = {channelID : channelID[i], discordChannelID, guildID, bot}
-                console.log(config.channelID, config.discordChannelID)
-                
-                const mixerBot = new mixer(config)
-                mixerBot.start()
-                mixerBot.ready(() => {
-                    console.log(`ready`)
-                })
-            }   
+                for(i = 0; i < result.length; i++) {
+                    SchannelIDS[guildID].push(Math.floor(result[i].streamerID))
+                }
+
+                let guildIDS = Object.keys(SchannelIDS)
+                for(i = 0; i < guildIDS.length; i++) {
+                    var channelID = SchannelIDS[guildIDS[i]]
+                    var discordChannelID = `${DchannelIDS[guildIDS[i]].aChannel}`
+                    var guildID = `${guildIDS[i]}`
+        
+                    for(i = 0; i < channelID.length; i++) {
+                        let config = {channelID : channelID[i], discordChannelID, guildID, bot}
+                        
+                        const mixerBot = new mixer(config)
+                        mixerBot.start()
+                        mixerBot.ready(() => {
+                            console.log(`ready`)
+                        })
+                    }   
+                }
+            })
         }
     })
 }
