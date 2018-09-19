99e789a67752f61a680f67bd7ddbd79ddccd2944
Hattyot <39061940+Hattyot@users.noreply.github.com>
2018-09-19 13:50:39 +0300

    Create eval.js

---
+++ cmds/dev/eval.js
@@ -0,0 +1,25 @@
+const Discord = require("discord.js")
+const moment = require("moment");
+module.exports.run = async (bot, message, args) => {
+    try {
+        const code = args.join(" ")
+        let evaled = eval(code)
+
+        if (typeof evaled !== "string")
+            evaled = require("util").inspect(evaled)
+
+        message.channel.send(evaled, {code:"xl"});
+    } catch (err) {
+        message.channel.send(`Error fam`);
+      }
+}
+
+module.exports.help = {
+    name: "eval",
+}
+
+module.exports.conf = {
+    enabled: true,
+    aliases: [],
+    cat: "dev"
+  };
