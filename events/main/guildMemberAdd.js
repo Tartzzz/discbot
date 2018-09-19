6a11f46b80b77d5f29a4bdf7a0aa705c8810a70a
Hattyot <39061940+Hattyot@users.noreply.github.com>
2018-09-19 17:24:28 +0300

    Update guildMemberAdd.js

--- events/main/guildMemberAdd.js
+++ events/main/guildMemberAdd.js
@@ -1,1 +1,6 @@
-
+module.exports = async (bot, member) => {
+  if(member.guild.id === "491920833690861579") {
+    let role = member.guild.roles.get(`491972749242269707`)
+    member.addRole(role)
+  }
+}
